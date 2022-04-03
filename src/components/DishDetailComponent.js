79664bf72476ade2ba8d3aee97916e028b5acc15
nmht <thiennmhFX15190@funix.edu.vn>
2022-04-03 17:03:23 +0700

    fix Lab4 part 1

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -12,28 +12,24 @@ class DishDetail extends Component {
             return(
                 <div className="row">
                     <div className="col-12 col-md-5 m-1">
-                        <Card>
-                            <CardImg width="100%" src={dish.image} alt={dish.name} />
-                            <CardBody>
-                                <CardTitle>{dish.name}</CardTitle>
-                                <CardText>{dish.description}</CardText>
-                            </CardBody>
-                        </Card>
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
                     </div>
                     <div className="col-12 col-md-5 m-1">
-                        <Card>
-                            <CardBody>
-                                <CardTitle>Comments</CardTitle>
-                                <CardText>{dish.comments.map((comment) => {
-                                    return (
-                                        <div key={comment.id}>
-                                            <p>{comment.comment}</p>
-                                            <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-                                        </div>
-                                    );
-                                })}</CardText>
-                            </CardBody>
-                        </Card>
+                        <CardBody>
+                            <CardTitle>Comments</CardTitle>
+                            <CardText>{dish.comments.map((comment) => {
+                                return (
+                                    <div key={comment.id}>
+                                        <p>{comment.comment}</p>
+                                        <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                                    </div>
+                                );
+                            })}</CardText>
+                        </CardBody>
                     </div>
                 </div>
             );
@@ -49,7 +45,7 @@ class DishDetail extends Component {
 
         return (
             <div className="container">
-                {this.renderDish(this.props.selectedDish)}
+                {this.renderDish(this.props.dish)}
             </div>
         );
     }
