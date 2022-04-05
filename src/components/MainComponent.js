8d599a0cb98f904c45baff3aaeeca88f55a47184
nmht <thiennmhFX15190@funix.edu.vn>
2022-04-05 14:03:52 +0700

    Lab5 part 3 SPA1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import Contact from './ContactComponent';
 // import DishDetail from './DishDetailComponent';
 import Header  from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 class Main extends Component {
@@ -14,6 +18,9 @@ class Main extends Component {
 
         this.state = {
             dishes: DISHES,
+            comments: COMMENTS,
+            leaders: LEADERS,
+            promotions: PROMOTIONS
         };
     }
 
@@ -23,8 +30,12 @@ class Main extends Component {
             <div>
                 <Header />
                 <Routes>
-                    <Route path="home" element={<Home />} />
+                    <Route path="home" element={<Home dish={this.state.dishes.filter((dish) => dish.featured)[0]} 
+                    promotion={this.state.promotions.filter((promotion) => promotion.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                    />} />
                     <Route exact path="menu" element={<Menu dishes={this.state.dishes} />} />
+                    <Route exact path="contactus" element={<Contact />} />
                     <Route path="*" element={<Navigate to="/home" replace />} />
                 </Routes>
                 <Footer />
