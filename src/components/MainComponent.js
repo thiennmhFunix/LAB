0dcdc4ee3ea320c910b45d9c2e878b3d0b4ec79d
nmht <thiennmhFX15190@funix.edu.vn>
2022-04-05 12:42:07 +0700

    Lab5 part 2 react router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,31 +1,32 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
-import DishDetail from './DishDetailComponent';
+// import DishDetail from './DishDetailComponent';
 import Header  from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 class Main extends Component {
 
     constructor(props) {
-    super(props);
+        super(props);
 
-    this.state = {
-        dishes: DISHES,
-        selectedDish: null
-    };
-    }
-
-    onDishSelect(dishId) {
-        this.setState({ selectedDish: dishId})
+        this.state = {
+            dishes: DISHES,
+        };
     }
 
     render() {
+
         return (
             <div>
                 <Header />
-                <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+                <Routes>
+                    <Route path="home" element={<Home />} />
+                    <Route exact path="menu" element={<Menu dishes={this.state.dishes} />} />
+                    <Route path="*" element={<Navigate to="/home" replace />} />
+                </Routes>
                 <Footer />
             </div>
         );
