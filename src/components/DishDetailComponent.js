85f87d54d79a473f7de757efdc5afbcf3ff6451b
nmht <thiennmhFX15190@funix.edu.vn>
2022-04-08 15:37:20 +0700

    Lab5 part 4 SPA 2

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,49 +1,59 @@
 import React from "react";
-import { CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-
-    function RenderDish({dish}) {
-        if (dish != null) {
-            return(
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        <CardImg width="100%" src={dish.image} alt={dish.name} />
-                        <CardBody>
-                            <CardTitle>{dish.name}</CardTitle>
-                            <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </div>
-                    <div className="col-12 col-md-5 m-1">
-                        <CardBody>
-                            <CardTitle>Comments</CardTitle>
-                            <CardText>{dish.comments.map((comment) => {
-                                return (
-                                    <div key={comment.id}>
-                                        <p>{comment.comment}</p>
-                                        <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-                                    </div>
-                                );
-                            })}</CardText>
-                        </CardBody>
-                    </div>
+function RenderDish(props) {
+    if (props.dish != null) {
+        return(
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    <CardImg width="100%" src={props.dish.image} alt={props.dish.name} />
+                    <CardBody>
+                        <CardTitle>{props.dish.name}</CardTitle>
+                        <CardText>{props.dish.description}</CardText>
+                    </CardBody>
                 </div>
-            );
-        }
-        else {
-            return(
-                <div></div>    
-            );
-        }
-    }
-
-    const DishDetail = (props) => {
-
-        return (
-            <div className="container">
-                <RenderDish dish={props.dish} />
+                <div className="col-12 col-md-5 m-1">
+                    <CardBody>
+                        <CardTitle>Comments</CardTitle>
+                        <CardText>{props.comments.map((comment) => {
+                            return (
+                                <div key={comment.id}>
+                                    <p>{comment.comment}</p>
+                                    <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                                </div>
+                            );
+                        })}</CardText>
+                    </CardBody>
+                </div>
             </div>
         );
     }
+    else {
+        return(
+            <div></div>    
+            );
+        }
+}
+
+const DishDetail = (props) => {
+
+    return (
+        <div className="container">
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr />
+                </div>
+            </div>
+            <RenderDish dish={props.dish} comments={props.comments} />
+        </div>
+    );
+}
 
 
 export default DishDetail;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
-// import DishDetail from './DishDetailComponent';
+import DishDetail from './DishDetailComponent';
 import Header  from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { LEADERS } from '../shared/leaders';
 import { PROMOTIONS } from '../shared/promotions';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate, useParams } from 'react-router-dom';
 
 class Main extends Component {
 
@@ -24,8 +24,18 @@ class Main extends Component {
         };
     }
 
-    render() {
+    
+    render() {       
+        
+        const DishWithId = () => {
 
+            const params = useParams();
+
+            return (
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(params.dishId, 10))[0]} comments={this.state.comments.filter((comment) => comment.dishId === parseInt(params.dishId, 10))} />
+            );
+        }
+        
         return (
             <div>
                 <Header />
@@ -35,6 +45,7 @@ class Main extends Component {
                     leader={this.state.leaders.filter((leader) => leader.featured)[0]}
                     />} />
                     <Route exact path="menu" element={<Menu dishes={this.state.dishes} />} />
+                    <Route path="menu/:dishId" element={<DishWithId />} />
                     <Route exact path="contactus" element={<Contact />} />
                     <Route path="*" element={<Navigate to="/home" replace />} />
                 </Routes>
